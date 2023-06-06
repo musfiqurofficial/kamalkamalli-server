c14510d4cb5b432ff2f5501090772e1ad0fccb13
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-06-06 13:11:28 +0600

    new code

--- index.js
+++ index.js
@@ -1,91 +1,64 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const PORT = 5000;
-const cors = require("cors")
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const io = require("socket.io")(server);
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
 
 app.use(cors());
 
-mongoose
-  .connect(
-    "mongodb+srv://musfiqurofficial1999:ncreynxPEgYdqnia@cluster0.4zjwycc.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+let elements = [];
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  io.to(socket.id).emit("whiteboard-state", elements);
+
+  socket.on("element-update", (elementData) => {
+    updateElementInElements(elementData);
+
+    socket.broadcast.emit("element-update", elementData);
   });
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  socket.on("whiteboard-clear", () => {
+    elements = [];
 
-const User = mongoose.model("User", userSchema);
+    socket.broadcast.emit("whiteboard-clear");
+  });
 
-app.post("/api/sign-up", async (req, res) => {
-  try {
-    const { fullName, email, password } = req.body;
-
-    // Check if the email already exists in the database
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: "Email already exists" });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user instance
-    const newUser = new User({
-      fullName,
-      email,
-      password: hashedPassword,
+  socket.on("cursor-position", (cursorData) => {
+    socket.broadcast.emit("cursor-position", {
+      ...cursorData,
+      userId: socket.id,
     });
+  });
 
-    // Save the user to the database
-    await newUser.save();
-
-    // Return a success response
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    // Return an error response
-    res.status(500).json({ message: "Internal server error" });
-  }
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", socket.id);
+  });
 });
 
-
-// Get all items
-app.get("/", async (req, res) => {
-  try {
-    res.send("Welcome");
-  } catch (error) {
-    console.error("Error retrieving items:", error);
-    res.status(500).json({ error: "Server error" });
-  }
+app.get("/", (req, res) => {
+  res.send("Hello server is working");
 });
 
-// Start the server
+const PORT = process.env.PORT || 3003;
+
 server.listen(PORT, () => {
-  // Use the server variable to start the server
-  console.log(`Server is listening on port ${PORT}`);
+  console.log("server is running on port", PORT);
 });
+
+const updateElementInElements = (elementData) => {
+  const index = elements.findIndex((element) => element.id === elementData.id);
+
+  if (index === -1) return elements.push(elementData);
+
+  elements[index] = elementData;
+};
